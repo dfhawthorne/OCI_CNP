b69499991602daf7b666b918be160ad203c3dfc4
Douglas Hawthorne <dfhawthorne@computer.org>
2024-11-29 11:15:50 +1100

    Added random number to OS bucket name

--- terraform/Lab_08_Infrastructure_Security_Network/ashburn_bucket.tf
+++ terraform/Lab_08_Infrastructure_Security_Network/ashburn_bucket.tf
@@ -21,9 +21,8 @@ resource "oci_identity_network_source" "IAD-NP-LAB08-NS-01" {
     							]
 }
 
-
 # ------------------------------------------------------------------------------
-# Create Object Storage Bucket
+# Get OS namespace and User name for OS bucket
 # ------------------------------------------------------------------------------
 
 data "oci_objectstorage_namespace" "IAD-NP-LAB08-namespace" {
@@ -36,13 +35,27 @@ data "oci_identity_user" "MyLearn-user" {
 	user_id                 = var.provider_details.user_ocid
 }
 
+# ------------------------------------------------------------------------------
+# Generate a random number for bucket name
+# ------------------------------------------------------------------------------
+
+resource "random_integer" "bucket_number" {
+	min 							=    1
+	max 							= 1000
+}
+
+# ------------------------------------------------------------------------------
+# Create Object Storage Bucket
+# ------------------------------------------------------------------------------
+
 locals {
     user_name               = data.oci_identity_user.MyLearn-user.name
+	bucket_number			= random_integer.bucket_number.result
 }
 
 resource "oci_objectstorage_bucket" "IAD-NP-LAB08-BUCKET" {
     provider                = oci.ashburn
 	compartment_id          = var.compartment_id
-	name                    = "IAD-NP-LAB08-BUCKET-${local.user_name}"
+	name                    = "IAD-NP-LAB08-BUCKET-${local.user_name}-${local.bucket_number}"
 	namespace               = data.oci_objectstorage_namespace.IAD-NP-LAB08-namespace.namespace
 }
