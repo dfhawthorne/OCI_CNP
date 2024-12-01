089f4f3494b3684edbe6791e630475566acc797d
Douglas Hawthorne <dfhawthorne@computer.org>
2024-12-01 17:35:10 +1100

    Fixed issue #18

--- terraform/Lab_09_Infrastructure_Security_Network_SSL/ashburn_vcn.tf
+++ terraform/Lab_09_Infrastructure_Security_Network_SSL/ashburn_vcn.tf
@@ -13,7 +13,7 @@ resource "oci_core_vcn" "IAD-NP-LAB09-VCN-01" {
     provider                    = oci.ashburn
     compartment_id              = var.compartment_id
     display_name                = "IAD-NP-LAB09-VCN-01"
-    cidr_blocks                 = ["10.0.0.0/24"]
+    cidr_blocks                 = ["10.0.0.0/16"]
     dns_label                   = "iadlab09vcn01"
 }
 
