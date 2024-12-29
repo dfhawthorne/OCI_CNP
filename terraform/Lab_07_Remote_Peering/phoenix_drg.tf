9f96416b44ebd38771ab653138ffa5de8f76e102
Douglas Hawthorne <dfhawthorne@computer.org>
2024-12-29 22:28:20 +1100

    Lab 07: Partial Solution

--- terraform/Lab_07_Remote_Peering/phoenix_drg.tf
+++ terraform/Lab_07_Remote_Peering/phoenix_drg.tf
@@ -31,11 +31,22 @@ resource "oci_core_drg_attachment" "PHX-NP-LAB06-VCN-01-ATCH" {
 	}
 }
 
-data "oci_core_drg_attachments" "PHX-NP-LAB06-DRG-ATTCH-01" {
-    provider                    = oci.phoenix
-	compartment_id              = var.compartment_id
-	drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
-}
+# The following resource fails with:
+# │ Error: unknown type 'REMOTE_PEERING_CONNECTION' was specified
+# │ 
+# │   with oci_core_drg_attachment.PHX-NP-LAB06-RPC-01-ATCH,
+# │   on phoenix_drg.tf line 35, in resource "oci_core_drg_attachment" "PHX-NP-LAB06-RPC-01-ATCH":
+# │   35: resource "oci_core_drg_attachment" "PHX-NP-LAB06-RPC-01-ATCH" {
+#resource "oci_core_drg_attachment" "PHX-NP-LAB06-RPC-01-ATCH" {
+#    provider                    = oci.phoenix
+#	drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
+#	display_name                = "DRG Attachment for RPC: PHX-NP-LAB07-RPC-01"
+#    drg_route_table_id          = oci_core_drg_route_table.PHX-NP-LAB07-RT-RPC-01.id
+#	network_details {
+#		id                      = oci_core_remote_peering_connection.PHX-NP-LAB07-RPC-01.id
+#		type                    = "REMOTE_PEERING_CONNECTION"
+#	}
+#}
 
 # ------------------------------------------------------------------------------
 # Create Route Distributions for DRG
@@ -88,16 +99,6 @@ resource "oci_core_drg_route_table" "PHX-NP-LAB07-RT-VPN-01" {
 	import_drg_route_distribution_id = oci_core_drg_route_distribution.PHX-NP-LAB07-RD-RPC-01.id
 }
 
-#resource "oci_core_drg_attachment_management" "PHX-NP-LAB07-ATTCH-VPN-01" {
-#    provider                    = oci.phoenix
-#    attachment_type             = "IPSEC_TUNNEL"
-#    compartment_id              = var.compartment_id
-#    network_id                  = data.oci_core_ipsec_connection_tunnels.PHX-NP-LAB06-VPN-01.ip_sec_connection_tunnels[0].id
-#    drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
-#    display_name                = "PHX-NP-LAB07-ATTCH-VPN-01"
-#    drg_route_table_id          = oci_core_drg_route_table.PHX-NP-LAB07-RT-VPN-01.id
-#}
-
 resource "oci_core_drg_route_table" "PHX-NP-LAB07-RT-RPC-01" {
     provider                    = oci.phoenix
 	drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
@@ -105,12 +106,37 @@ resource "oci_core_drg_route_table" "PHX-NP-LAB07-RT-RPC-01" {
 	import_drg_route_distribution_id = oci_core_drg_route_distribution.PHX-NP-LAB07-RD-VPN-01.id
 }
 
-#resource "oci_core_drg_attachment_management" "PHX-NP-LAB07-ATTCH-RPC-01" {
-#    provider                    = oci.phoenix
-#    attachment_type             = "REMOTE_PEERING_CONNECTION"
-#    compartment_id              = var.compartment_id
-#    network_id                  = oci_core_remote_peering_connection.PHX-NP-LAB07-RPC-01.id
-#    drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
-#    display_name                = "PHX-NP-LAB07-ATTCH-RPC-01"
-#    drg_route_table_id          = oci_core_drg_route_table.PHX-NP-LAB07-RT-RPC-01.id
-#}
+# ------------------------------------------------------------------------------
+# Update DRG Attachments with new route tables 
+# ------------------------------------------------------------------------------
+
+resource "oci_core_drg_attachment_management" "PHX-NP-LAB07-DA-RPC-01" {
+    provider                    = oci.phoenix
+    attachment_type             = "REMOTE_PEERING_CONNECTION"
+    compartment_id              = var.compartment_id
+    network_id                  = oci_core_remote_peering_connection.PHX-NP-LAB07-RPC-01.id
+    drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
+    display_name                = "PHX-NP-LAB07-DA-RPC-01"
+  drg_route_table_id            = oci_core_drg_route_table.PHX-NP-LAB07-RT-RPC-01.id
+}
+
+resource "oci_core_drg_attachment_management" "PHX-NP-LAB07-DA-VPN-01" {
+    provider                    = oci.phoenix
+    attachment_type             = "IPSEC_TUNNEL"
+    compartment_id              = var.compartment_id
+    network_id                  = data.oci_core_ipsec_connection_tunnels.PHX-NP-LAB06-VPN-01.ip_sec_connection_tunnels[0].id
+    drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
+    display_name                = "PHX-NP-LAB07-DA-VPN-01"
+    drg_route_table_id          = oci_core_drg_route_table.PHX-NP-LAB07-RT-RPC-01.id
+}
+
+resource "oci_core_drg_attachment_management" "PHX-NP-LAB07-DA-VPN-02" {
+    provider                    = oci.phoenix
+    attachment_type             = "IPSEC_TUNNEL"
+    compartment_id              = var.compartment_id
+    network_id                  = data.oci_core_ipsec_connection_tunnels.PHX-NP-LAB06-VPN-01.ip_sec_connection_tunnels[1].id
+    drg_id                      = oci_core_drg.PHX-NP-LAB06-DRG-01.id
+    display_name                = "PHX-NP-LAB07-DA-VPN-02"
+    drg_route_table_id          = oci_core_drg_route_table.PHX-NP-LAB07-RT-RPC-01.id
+}
+
