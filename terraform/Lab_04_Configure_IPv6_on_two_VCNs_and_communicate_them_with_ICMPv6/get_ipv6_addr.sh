bc6b313b4094c53a53083caca326b1e70588317f
Douglas Hawthorne <dfhawthorne@computer.org>
2025-02-16 23:12:24 +1100

    Lab 04: updated get_ipv6_addr.sh to only consider RUNNING COMPUTE instances

--- terraform/Lab_04_Configure_IPv6_on_two_VCNs_and_communicate_them_with_ICMPv6/get_ipv6_addr.sh
+++ terraform/Lab_04_Configure_IPv6_on_two_VCNs_and_communicate_them_with_ICMPv6/get_ipv6_addr.sh
@@ -5,8 +5,8 @@
 
 oci_session.sh
 
-vm_01_id=$(oci compute instance list --query 'data[0].id' --raw-output)
-vm_02_id=$(oci compute instance list --query 'data[1].id' --raw-output)
+vm_01_id=$(oci compute instance list --query 'data[0].id' --raw-output --lifecycle-state RUNNING)
+vm_02_id=$(oci compute instance list --query 'data[1].id' --raw-output --lifecycle-state RUNNING)
 vm_01_vnic_id=$(oci compute instance list-vnics --instance-id ${vm_01_id} --all --query 'data[0].id' --raw-output)
 vm_02_vnic_id=$(oci compute instance list-vnics --instance-id ${vm_02_id} --all --query 'data[0].id' --raw-output)
 vm_01_ipv6=$(oci network ipv6 list --vnic-id ${vm_01_vnic_id} --query 'data[0]."ip-address"' --raw-output)
